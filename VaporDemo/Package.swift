40e071957843f825a333cb7e6c868ebd1c1ea2c2
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-04-05 02:05:46 +0100

    Add VaporDemo

---
+++ VaporDemo/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "vapor-demo",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+        .package(path: "../../meilisearch-swift")
+    ],
+    targets: [
+        .target(name: "App", dependencies: [
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "MeiliSearch", package: "meilisearch-swift")
+        ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
---
+++ VaporDemo/Sources/App/routes.swift
@@ -0,0 +1,44 @@
+import Vapor
+import MeiliSearch
+
+func routes(_ app: Application) throws {
+
+    let client = MeiliSearchClient(Config(hostURL: "http://localhost:7700"))
+
+    app.get("index") { req -> EventLoopFuture<String> in
+
+        /// Create a new void promise
+        let promise = req.eventLoop.makePromise(of: String.self)
+
+        /// Dispatch some work to happen on a background thread
+        req.application.threadPool.submit { _ in
+
+            guard let uid: String = req.query["uid"] else {
+                promise.fail(Abort(.badRequest))
+                return
+            }
+
+            client.getIndex(uid: uid) { result in
+
+              switch result {
+              case .success(let index):
+
+                let encoder = JSONEncoder()
+                let data = try! encoder.encode(index)
+                let dataString = String(decoding: data, as: UTF8.self)
+
+                promise.succeed(dataString)
+
+              case .failure(let error):
+                promise.fail(error)
+              }
+
+            }
+
+        }
+
+        /// Wait for the future to be completed,
+        /// then transform the result to a simple String
+        return promise.futureResult
+    }
+}
