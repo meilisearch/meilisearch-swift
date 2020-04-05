ca01109c5c63669d197fd998430892744a606001
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-04-05 04:33:01 +0100

    Make processingTimeMs optional

--- Sources/MeiliSearch/Model/SearchResult.swift
+++ Sources/MeiliSearch/Model/SearchResult.swift
@@ -4,7 +4,7 @@ public struct SearchResult {
     public let hits: [[String: Any]]
     public let offset: Int
     public let limit: Int
-    public let processingTimeMs: Int
+    public let processingTimeMs: Int?
     public let query: String
 
     init(json: Any) {
@@ -14,14 +14,14 @@ public struct SearchResult {
         hits = decodeHits(json: dic["hits"])
         offset = safeDictionaryEntry(dic, "offset", 0)
         limit = safeDictionaryEntry(dic, "limit", 0)
-        processingTimeMs = dic["processingTimeMs"] as! Int
+        processingTimeMs = dic["processingTimeMs"] as? Int
         query = dic["query"] as! String
     }
 
 }
 
 private func safeDictionaryEntry<T>(_ dic: [String: Any], _ key: String, _ defaultValue: T) -> T {
-    let entry: Any = dic[key]
+    let entry: Any? = dic[key]
     if let entry = entry as? T {
         return entry
     }
