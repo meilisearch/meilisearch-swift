0944403698f5d2b69201bef5928502b88a327134
Damien Rivet <damien.rivet@gmail.com>
2020-10-02 15:48:47 +0200

    [48] Remove acceptNewFields routes
    Cleaned up Setting struct and added default values for attributes.
    Added UT for testing default values.
    Removed "distinctAttributes" from JSON test payload.
    
    Signed-off-by: Damien Rivet <damien.rivet@gmail.com>

--- Sources/MeiliSearch/Model/Setting.swift
+++ Sources/MeiliSearch/Model/Setting.swift
@@ -3,53 +3,42 @@ import Foundation
 /**
  Each instance of MeiliSearch has three keys: a master, a private, and a public.
  Each key has a given set of permissions on the API routes.
-*/
+ */
 public struct Setting: Codable, Equatable {
 
     // MARK: Properties
 
-    ///List of ranking rules for a given `Index`.
+    /// List of ranking rules for a given `Index`.
     public let rankingRules: [String]
 
-    ///List of searchable attributes for a given `Index`.
+    /// List of searchable attributes for a given `Index`.
     public let searchableAttributes: [String]
 
-    ///List of displayed attributes for a given `Index`.
+    /// List of displayed attributes for a given `Index`.
     public let displayedAttributes: [String]
 
-    ///List of stop-words for a given `Index`.
+    /// List of stop-words for a given `Index`.
     public let stopWords: [String]
 
-    ///List of synonyms and its values for a given `Index`.
+    /// List of synonyms and its values for a given `Index`.
     public let synonyms: [String: [String]]
 
-    ///Return if a given `Index` allows new fields.
-    public let acceptNewFields: Bool
-
-    public init(
-      rankingRules: [String],
-      searchableAttributes: [String],
-      displayedAttributes: [String],
-      stopWords: [String],
-      synonyms: [String: [String]],
-      acceptNewFields: Bool) {
-      self.rankingRules = rankingRules
-      self.searchableAttributes = searchableAttributes
-      self.displayedAttributes = displayedAttributes
-      self.stopWords = stopWords
-      self.synonyms = synonyms
-      self.acceptNewFields = acceptNewFields
+    public init(rankingRules: [String], searchableAttributes: [String], displayedAttributes: [String], stopWords: [String], synonyms: [String: [String]]) {
+        self.rankingRules = rankingRules
+        self.searchableAttributes = searchableAttributes
+        self.displayedAttributes = displayedAttributes
+        self.stopWords = stopWords
+        self.synonyms = synonyms
     }
 
-    ///Tries to decode the JSON object to Setting object.
+    /// Tries to decode the JSON object to Setting object.
     public init(from decoder: Decoder) throws {
         let values = try? decoder.container(keyedBy: CodingKeys.self)
+
         rankingRules = (try? values?.decodeIfPresent([String].self, forKey: .rankingRules)) ?? []
-        searchableAttributes = (try? values?.decodeIfPresent([String].self, forKey: .searchableAttributes)) ?? []
-        displayedAttributes = (try? values?.decodeIfPresent([String].self, forKey: .displayedAttributes)) ?? []
+        searchableAttributes = (try? values?.decodeIfPresent([String].self, forKey: .searchableAttributes)) ?? ["*"]
+        displayedAttributes = (try? values?.decodeIfPresent([String].self, forKey: .displayedAttributes)) ?? ["*"]
         stopWords = (try? values?.decodeIfPresent([String].self, forKey: .stopWords)) ?? []
         synonyms = (try? values?.decodeIfPresent([String: [String]].self, forKey: .synonyms)) ?? [:]
-        acceptNewFields = (try? values?.decodeIfPresent(Bool.self, forKey: .acceptNewFields)) ?? false
     }
-
 }
