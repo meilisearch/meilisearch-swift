57775fd83453c01686aa093dfbc89542521dd4a0
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-06-29 19:56:42 +0200

    Working error handler with all error types

--- Sources/MeiliSearch/Error.swift
+++ Sources/MeiliSearch/Error.swift
@@ -6,19 +6,54 @@ import Foundation
 public extension MeiliSearch {
 
   // MARK: Error
-  public struct MSErrorResponse: Decodable, Equatable {
+  struct MSErrorResponse: Decodable, Equatable {
     public let message: String
     public let errorCode: String
     public let errorType: String
     public let errorLink: String?
   }
 
+  static func errorHandler(url: URL, data: Data?, response: URLResponse?, error: Swift.Error?) throws {
+
+    // Communication Error with MeiliSearch
+    if let error: Swift.Error = error {
+      throw MeiliSearch.Error.meiliSearchCommunicationError(
+        message: error.localizedDescription,
+        url: url.absoluteString
+      )
+    }
+
+    guard let response = response as? HTTPURLResponse else {
+      fatalError("Correct handles invalid response, please create a custom error type")
+    }
+
+    // Error returned by MeiliSearch
+    if let unwrappedData: Data = data, let res: MeiliSearch.MSErrorResponse = try? Constants.customJSONDecoder.decode(MeiliSearch.MSErrorResponse.self, from: unwrappedData) {
+      throw MeiliSearch.Error.meiliSearchApiError(
+        message: res.message,
+        msErrorResponse: MeiliSearch.MSErrorResponse(
+          message: res.message,
+          errorCode: res.errorCode,
+          errorType: res.errorType,
+          errorLink: res.errorLink
+        ),
+        statusCode: response.statusCode,
+        url: url.absoluteString                            )
+    }
+
+    // HTTP error with MeiliSearch
+    if 400 ... 599 ~= response.statusCode {
+      throw MeiliSearch.Error.meiliSearchApiError(
+        message: HTTPURLResponse.localizedString(forStatusCode: response.statusCode),
+        msErrorResponse: nil,
+        statusCode: response.statusCode,
+        url: url.absoluteString
+      )
+    }
+  }
+
   /// Generic Error types for MeiliSearch,
-<<<<<<< HEAD
   public enum Error: Swift.Error, LocalizedError, Equatable {
-=======
-  enum Error: Swift.Error, LocalizedError {
->>>>>>> Create error enum
 
     /// The client tried to contact the server but it was not found.
     case serverNotFound
@@ -32,7 +67,6 @@ public extension MeiliSearch {
     /// The input or output JSON is invalid.
     case invalidJSON
 
-<<<<<<< HEAD
     // URL is invalid
     case invalidURL
 
@@ -42,13 +76,6 @@ public extension MeiliSearch {
 
     /// Error communicating with MeiliSearch API.
     case meiliSearchCommunicationError(message: String, url: String)
-=======
-    /// Error originating from MeiliSearch API.
-    case meiliSearchApiError(message: String, errorCode: String, errorType: String, errorLink: String? = "http://docs.meilisearch.com/errors", underlying: Swift.Error)
-
-    /// Error communicating with MeiliSearch API.
-    case meiliSearchCommunicationError
->>>>>>> Create error enum
 
     public var errorDescription: String? {
       switch self {
@@ -60,7 +87,6 @@ public extension MeiliSearch {
         return "Response decoding failed"
       case .invalidJSON:
         return "Invalid json"
-<<<<<<< HEAD
       case .invalidURL:
         return "Invalid host URL"
       case .meiliSearchCommunicationError(let message, let url):
@@ -69,16 +95,17 @@ public extension MeiliSearch {
       // can we do let error and then error.message etc...
       case .meiliSearchApiError(let message, let error, let statusCode, let url):
         if let msErrorResponse = error as? MSErrorResponse {
-         return "MeiliSearchApiError: \(msErrorResponse.message) \n errorCode: \(msErrorResponse.errorCode) \n errorType: \(msErrorResponse.errorType) \n errorLink: \(String(describing: msErrorResponse.errorLink)) \n status: \(statusCode) "
+          return """
+          MeiliSearchApiError: \(msErrorResponse.message)
+          errorCode: \(msErrorResponse.errorCode)
+          errorType: \(msErrorResponse.errorType)
+          errorLink: \(String(describing: msErrorResponse.errorLink))
+          status: \(statusCode)
+          """
         }
         return "MeiliSearchApiError: \(String(describing: message)) - \(String(describing: statusCode)) - \(String(describing: url))"
-=======
-      case .meiliSearchCommunicationError:
-        return "Error communicating with MeiliSearch"
-      case .meiliSearchApiError(let message, let errorCode, let errorType, let errorLink, let underlying):
-        return "All hosts are unreachable. message: \(message) errorcode: \(errorCode) \(errorType) \(errorLink) \(underlying)"
->>>>>>> Create error enum
       }
     }
   }
 }
+{"mode":"full","isActive":false}
