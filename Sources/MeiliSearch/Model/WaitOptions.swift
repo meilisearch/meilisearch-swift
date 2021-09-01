5e5335cf2eff8b695e57bf23c30b538db7b92667
Charlotte Vermandel <charlottevermandel@gmail.com>
2021-09-01 17:06:34 +0200

    Improve typing of waitOptions

--- Sources/MeiliSearch/Model/WaitOptions.swift
+++ Sources/MeiliSearch/Model/WaitOptions.swift
@@ -7,19 +7,19 @@ public struct WaitOptions: Codable, Equatable {
 
   // MARK: Properties
 
-  /// Maximum time in seconds before cancel
+  /// Maximum time in seconds before timeOut
   public let timeOut: Double
 
   /// Interval in seconds between each status call
-  public let interval: Double
+  public let interval: TimeInterval
 
   // MARK: Initializers
   public init(
     timeOut: Double? = 5,
-    interval: Double? = 0.05
+    interval: TimeInterval? = 0.5
   ) {
     self.timeOut = timeOut ?? 5
-    self.interval = interval ?? 0.05
+    self.interval = interval ?? 0.5
   }
 
   // MARK: Codable Keys
