54c1526fb38f8eea87fc43ed8226299c805228d7
Clémentine Urquizar <clementine@meilisearch.com>
2022-01-05 17:15:45 +0100

    Add version of package in the code-samples

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -4,12 +4,15 @@
 current_tag=$(echo $GITHUB_REF | tr -d 'refs/tags/')
 file1='MeiliSearch.podspec'
 file2='README.md'
+file3='.code-samples.meilisearch.yaml'
 file_tag1=$(grep '  s.version' $file1 | cut -d '=' -f2 | tr -d "'" | tr -d ' ')
 file_tag2=$(grep '.package(url:' $file2 | cut -d ':' -f4 | tr -d '"' | tr -d ')' | tr -d ' ')
-if [ "$current_tag" != "$file_tag1" ] || [ "$current_tag" != "$file_tag2" ]; then
+file_tag3=$(grep '.package(url:' $file3 | cut -d ':' -f4 | tr -d '"' | tr -d ')' | tr -d ' ')
+if [ "$current_tag" != "$file_tag1" ] || [ "$current_tag" != "$file_tag2" ] || [ "$current_tag" != "$file_tag3" ]; then
   echo "Error: the current tag does not match the version in package file(s)."
   echo "$file1: found $file_tag1 - expected $current_tag"
   echo "$file2: found $file_tag2 - expected $current_tag"
+  echo "$file2: found $file_tag3 - expected $current_tag"
   exit 1
 fi
 
