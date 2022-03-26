1f01866580595e36adb02f5f1ec195b57f4e0f02
Bruno Casali <brunoocasali@gmail.com>
2022-03-26 14:53:32 -0300

    Create two support models Book, Movie to share between tests

---
+++ Tests/MeiliSearchIntegrationTests/Support/Movie.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+public struct Movie: Codable, Equatable {
+  let id: Int
+  let title: String
+  let comment: String?
+
+  init(id: Int, title: String, comment: String? = nil) {
+    self.id = id
+    self.title = title
+    self.comment = comment
+  }
+}
