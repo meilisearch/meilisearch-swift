44c76b808e7cdc7b710b1c80dc82088861fd9b2a
Bruno Casali <brunoocasali@gmail.com>
2022-03-29 23:11:57 -0300

    Create a internal struct to deliver the package version
    This includes both current "1.0.1" and qualifiedVersion "Meilisearch Swift (v1.0.1)"

---
+++ Sources/MeiliSearch/Model/PackageVersion.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+internal struct PackageVersion {
+  /// This is the current version of the meilisearch-swift package
+  private static let current = "0.13.0"
+
+  /**
+   Retrieves the current version of the MeiliSearch Swift package and formats accordingly.
+
+   - returns: String containing the qualified version of this package eg. `Meilisearch Swift (v1.0.0)`
+   */
+  static func qualifiedVersion(_ rawVersion: String? = nil) -> String {
+    "Meilisearch Swift (v\(rawVersion ?? self.current))"
+  }
+}
