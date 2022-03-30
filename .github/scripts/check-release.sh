70e5e29424d731a5f0645769d237958ee568064a
Bruno Casali <brunoocasali@gmail.com>
2022-03-29 23:19:18 -0300

    Add a new check-release step to prevent issues with the new versioning

--- .github/scripts/check-release.sh
+++ .github/scripts/check-release.sh
@@ -5,14 +5,18 @@ current_tag=$(echo $GITHUB_REF | tr -d 'refs/tags/')
 file1='MeiliSearch.podspec'
 file2='README.md'
 file3='.code-samples.meilisearch.yaml'
+file4='Sources/MeiliSearch/Model/PackageVersion.swift'
 file_tag1=$(grep '  s.version' $file1 | cut -d '=' -f2 | tr -d "'" | tr -d ' ')
 file_tag2=$(grep '.package(url:' $file2 | cut -d ':' -f4 | tr -d '"' | tr -d ')' | tr -d ' ')
 file_tag3=$(grep '.package(url:' $file3 | cut -d ':' -f4 | tr -d '"' | tr -d ')' | tr -d ' ')
-if [ "$current_tag" != "$file_tag1" ] || [ "$current_tag" != "$file_tag2" ] || [ "$current_tag" != "$file_tag3" ]; then
+file_tag4=$(grep 'let current' -A 0 $file4 | cut -d '=' -f2 | tr -d '"' | tr -d ' ')
+
+if [ "$current_tag" != "$file_tag1" ] || [ "$current_tag" != "$file_tag2" ] || [ "$current_tag" != "$file_tag3" ] || [ "$current_tag" != "$file_tag4" ]; then
   echo "Error: the current tag does not match the version in package file(s)."
   echo "$file1: found $file_tag1 - expected $current_tag"
   echo "$file2: found $file_tag2 - expected $current_tag"
-  echo "$file2: found $file_tag3 - expected $current_tag"
+  echo "$file3: found $file_tag3 - expected $current_tag"
+  echo "$file4: found $file_tag4 - expected $current_tag"
   exit 1
 fi
 
