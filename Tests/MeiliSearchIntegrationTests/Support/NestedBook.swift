27f5d8bd313a2af5c37affd4ca467f54a02eb340
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-04-26 17:38:49 +0200

    Ensure nested field support

---
+++ Tests/MeiliSearchIntegrationTests/Support/NestedBook.swift
@@ -0,0 +1,40 @@
+public struct NestedBook: Codable, Equatable {
+  let id: Int
+  let title: String
+  let info: InfoNested
+  let genres: [String]?
+  let formatted: FormattedNestedBook?
+
+  enum CodingKeys: String, CodingKey {
+    case id
+    case title
+    case info
+    case genres
+    case formatted = "_formatted"
+  }
+
+  init(id: Int, title: String, info: InfoNested, genres: [String] = [], formatted: FormattedNestedBook? = nil) {
+    self.id = id
+    self.title = title
+    self.info = info
+    self.genres = genres
+    self.formatted = formatted
+  }
+}
+
+public struct InfoNested: Codable, Equatable {
+  let comment: String
+  let reviewNb: Int
+}
+
+public struct FormattedNestedBook: Codable, Equatable {
+  let id: String
+  let title: String
+  let info: InfoNested
+
+  init(id: Int, title: String, info: InfoNested) {
+    self.id = String(id)
+    self.title = title
+    self.info = info
+  }
+}
