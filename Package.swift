40d5548a15e0d655e883e35b408b4f2d31f4547c
Bruno Casali <brunoocasali@gmail.com>
2022-05-08 17:24:58 +0200

    Remove all related to the generateTenantToken method

--- Package.swift
+++ Package.swift
@@ -4,27 +4,18 @@ import PackageDescription
 
 let package = Package(
   name: "meilisearch-swift",
-  platforms: [
-    .iOS(.v12),
-    .tvOS(.v12),
-    .watchOS(.v5),
-    .macOS(.v10_15)
-  ],
   products: [
     .library(name: "MeiliSearch", targets: ["MeiliSearch"])
   ],
   dependencies: [
     // Support for dependabot for swift packages in dependabot
     // is in draft mode https://github.com/dependabot/dependabot-core/pull/3772
-    .package(url: "https://github.com/realm/SwiftLint.git", from: "0.43.1"),
-    .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
+    .package(url: "https://github.com/realm/SwiftLint.git", from: "0.43.1")
   ],
   targets: [
     .target(
       name: "MeiliSearch",
-      dependencies: [
-        .product(name: "JWTKit", package: "jwt-kit")
-      ]
+      dependencies: []
     ),
     .testTarget(
       name: "MeiliSearchUnitTests",
