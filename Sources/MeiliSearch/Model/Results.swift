9b2866f88c61ac1d9b936798fb9806feb83901d1
Bruno Casali <brunoocasali@gmail.com>
2022-08-03 11:16:55 -0300

    Remove Results type

--- Sources/MeiliSearch/Model/Results.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/**
- `Results` is a wrapper used in multiple routes returning an array of data.
- */
-
-public struct Results<T: Decodable & Encodable & Equatable>: Codable, Equatable {
-  public let results: [T]
-}
