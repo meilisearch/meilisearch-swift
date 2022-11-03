ccc905cd232705d599ab3d6117b17d3f55782116
Bruno Casali <brunoocasali@gmail.com>
2022-11-03 16:04:32 -0300

    Add blank line to fix linting issue

--- Tests/MeiliSearchUnitTests/KeyParamsTests.swift
+++ Tests/MeiliSearchUnitTests/KeyParamsTests.swift
@@ -3,28 +3,32 @@
 import XCTest
 
 class KeyParamsTests: XCTestCase {
-  func testInitializer() {
-    let keyParams1 = KeyParams(actions: ["action1"], indexes: ["index1"], expiresAt: nil)
-    XCTAssertEqual(keyParams1.actions, ["action1"])
-    XCTAssertEqual(keyParams1.indexes, ["index1"])
-    XCTAssertNil(keyParams1.expiresAt)
-    XCTAssertNil(keyParams1.description)
-    XCTAssertNil(keyParams1.name)
-    XCTAssertNil(keyParams1.uid)
-    
-    let keyParams2 = KeyParams(
-        description: "The default search key",
-        name: "Default Search Key",
-        uid: "74c9c733-3368-4738-bbe5-1d18a5fecb37",
-        actions: ["action1"],
-        indexes: ["index1"],
-        expiresAt: "2021-08-11T10:00:00Z"
+  func testCompleteInitializer() {
+    let keyParams = KeyParams(
+      description: "The default search key",
+      name: "Default Search Key",
+      uid: "74c9c733-3368-4738-bbe5-1d18a5fecb37",
+      actions: ["action1"],
+      indexes: ["index1"],
+      expiresAt: "2021-08-11T10:00:00Z"
     )
-    XCTAssertEqual(keyParams2.actions, ["action1"])
-    XCTAssertEqual(keyParams2.indexes, ["index1"])
-    XCTAssertEqual(keyParams2.expiresAt, "2021-08-11T10:00:00Z")
-    XCTAssertEqual(keyParams2.description, "The default search key")
-    XCTAssertEqual(keyParams2.name, "Default Search Key")
-    XCTAssertEqual(keyParams2.uid, "74c9c733-3368-4738-bbe5-1d18a5fecb37")
+
+    XCTAssertEqual(keyParams.actions, ["action1"])
+    XCTAssertEqual(keyParams.indexes, ["index1"])
+    XCTAssertEqual(keyParams.expiresAt, "2021-08-11T10:00:00Z")
+    XCTAssertEqual(keyParams.description, "The default search key")
+    XCTAssertEqual(keyParams.name, "Default Search Key")
+    XCTAssertEqual(keyParams.uid, "74c9c733-3368-4738-bbe5-1d18a5fecb37")
+  }
+
+  func testCompactInitializer() {
+    let keyParams = KeyParams(actions: ["action1"], indexes: ["index1"], expiresAt: nil)
+
+    XCTAssertEqual(keyParams.actions, ["action1"])
+    XCTAssertEqual(keyParams.indexes, ["index1"])
+    XCTAssertNil(keyParams.expiresAt)
+    XCTAssertNil(keyParams.description)
+    XCTAssertNil(keyParams.name)
+    XCTAssertNil(keyParams.uid)
   }
 }
